29248548a2cb131463aea7eb871e1477a06b0770
Cathleah Tabaquero <92615682+CathleahTelib@users.noreply.github.com>
2021-11-22 10:19:10 +0800

    Update Numbersorter.py

--- Numbersorter.py
+++ Numbersorter.py
@@ -67,7 +67,7 @@ elif n3>n1 and n3>n2 and n3>n4:
                 print(n2)
                 print(n4)
             else:
-                print( n4)
+                print(n4)
                 print(n2)
         elif n2>n1 and n2>n4:
             print(n2)
@@ -91,7 +91,7 @@ elif n4>n1 and n4>n2 and n4>n3:
         if n1>n2 and n1>n3:
                 print(n1)
                 if n2>n3:
-                    print(2)
+                    print(n2)
                     print(n3)
                 else:
                     print(n3)
